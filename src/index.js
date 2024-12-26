4a81bcfc64837714e987577808b7e4dbabc8bf48
Raghav Thapar <raghavthapar.apps@gmail.com>
2024-12-26 22:41:47 +0530

    Adding styles to the app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,110 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+
+// const pizzaData = [
+//   {
+//     name: "Focaccia",
+//     ingredients: "Bread with italian olive oil and rosemary",
+//     price: 6,
+//     photoName: "pizzas/focaccia.jpg",
+//     soldOut: false,
+//   },
+//   {
+//     name: "Pizza Margherita",
+//     ingredients: "Tomato and mozarella",
+//     price: 10,
+//     photoName: "pizzas/margherita.jpg",
+//     soldOut: false,
+//   },
+//   {
+//     name: "Pizza Spinaci",
+//     ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+//     price: 12,
+//     photoName: "pizzas/spinaci.jpg",
+//     soldOut: false,
+//   },
+//   {
+//     name: "Pizza Funghi",
+//     ingredients: "Tomato, mozarella, mushrooms, and onion",
+//     price: 12,
+//     photoName: "pizzas/funghi.jpg",
+//     soldOut: false,
+//   },
+//   {
+//     name: "Pizza Salamino",
+//     ingredients: "Tomato, mozarella, and pepperoni",
+//     price: 15,
+//     photoName: "pizzas/salamino.jpg",
+//     soldOut: true,
+//   },
+//   {
+//     name: "Pizza Prosciutto",
+//     ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+//     price: 18,
+//     photoName: "pizzas/prosciutto.jpg",
+//     soldOut: false,
+//   },
+// ];
 
 // The name of this function should always start with a capital letter
+
 function App() {
-  return <h1>Hello React!</h1>;
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+// Shows the information about the pizza
+function Pizza() {
+  return (
+    <div className="pizza">
+      <img src="pizzas/spinaci.jpg" alt="Spinaci Pizza" />
+      <h3>Pizza</h3>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+  );
+}
+
+// Contains all the pizzas fetched from data
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </main>
+  );
+}
+
+// Header component
+function Header() {
+  return (
+    <>
+      <header className="header">
+        <h1>Fast React Pizza Co.</h1>
+      </header>
+    </>
+  );
+}
+
+// Footer Component
+function Footer() {
+  return (
+    <footer className="footer">Welcome to Gazola's Pizza. We are open!</footer>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    {
-      "React strict mode renders the component twice and helps us know if there are any bugs or if we are using any outdated react API things"
-    }
+    {/* React strict mode renders the component twice and helps us know if there are any bugs or if we are using any outdated react API things */}
   </React.StrictMode>
 );
